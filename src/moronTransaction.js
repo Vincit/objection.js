c8e1e6bc51f0e0eb8052ceef33d57428f6cda086
Sami Koskimäki <sami.koskimaki@vincit.fi>
2015-06-20 19:21:43 +0300

    moron.transaction now throws if one of the given models is not a subclass of MoronModel

--- src/moronTransaction.js
+++ src/moronTransaction.js
@@ -11,26 +11,27 @@ var utils = require('./moronUtils');
  * @function transaction
  * @returns {Promise}
  */
-module.exports = function moronTransaction() {
+module.exports = function transaction() {
   // There must be at least one model class and the callback.
   if (arguments.length < 2) {
-    return Promise.reject(new Error('transaction: provide at least one MoronModel class to bind to the transaction'));
+    return Promise.reject(new Error('moron.transaction: provide at least one MoronModel class to bind to the transaction'));
   }
 
   // The last argument should be the callback and all other MoronModel subclasses.
   var callback = _.last(arguments);
   var modelClasses = _.take(arguments, arguments.length - 1);
-  var knex = _.first(modelClasses).knex();
+  var i;
 
-  for (var i = 0; i < modelClasses.length; ++i) {
-    var modelClass = modelClasses[i];
-
-    if (!utils.isSubclassOf(modelClass, MoronModel)) {
-      return Promise.reject(new Error('transaction: all but the last argument should be MoronModel subclasses'));
+  for (i = 0; i < modelClasses.length; ++i) {
+    if (!utils.isSubclassOf(modelClasses[i], MoronModel)) {
+      return Promise.reject(new Error('moron.transaction: all but the last argument should be MoronModel subclasses'));
     }
+  }
 
-    if (modelClass.knex() !== knex) {
-      return Promise.reject(new Error('transaction: all MoronModel subclasses must be bound to the same database'));
+  var knex = _.first(modelClasses).knex();
+  for (i = 0; i < modelClasses.length; ++i) {
+    if (modelClasses[i].knex() !== knex) {
+      return Promise.reject(new Error('moron.transaction: all MoronModel subclasses must be bound to the same database'));
     }
   }
 
