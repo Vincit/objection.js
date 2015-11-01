2cfd65c4367466ef8feee8db87ce83c595178f74
Sami Koskimäki <sami.koskimaki@vincit.fi>
2015-11-01 13:15:15 +0200

    bugfix: 'inline' ES6 inheritance of ES6 classes crashes beacuse super() cannot be called

--- tests/integration/insert.js
+++ tests/integration/insert.js
@@ -2,6 +2,7 @@ var _ = require('lodash');
 var utils = require('../../lib/utils')
 var expect = require('expect.js');
 var Promise = require('bluebird');
+var inheritModel = require('../../lib/inheritModel');
 var ValidationError = require('../../lib/ValidationError');
 
 module.exports = function (session) {
@@ -716,10 +717,7 @@ module.exports = function (session) {
     });
 
     function subClassWithSchema(Model, schema) {
-      function SubModel() {
-        Model.apply(this, arguments);
-      }
-      Model.extend(SubModel);
+      var SubModel = inheritModel(Model);
       SubModel.jsonSchema = schema;
       return SubModel;
     }
