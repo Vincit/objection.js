584e1ef8e003480c5c22fcb2e69a0b857e08a530
Sami Koskimäki <sami.koskimaki@vincit.com>
2015-12-22 03:39:52 +0200

    es7ify EagerFetcher

--- src/queryBuilder/EagerFetcher.js
+++ src/queryBuilder/EagerFetcher.js
@@ -1,93 +1,87 @@
-'use strict';
-
-var _ = require('lodash')
-  , Promise = require('bluebird')
-  , ValidationError = require('./../ValidationError')
-  , RelationExpression = require('./RelationExpression');
+import _ from 'lodash';
+import Promise from 'bluebird';
+import ValidationError from '../ValidationError';
+import RelationExpression from './RelationExpression';
 
 /**
- * @constructor
  * @ignore
  */
-function EagerFetcher(opt) {
-  this.modelClass = opt.modelClass;
-  this.models = opt.models;
-  this.eager = opt.eager;
-  this.filters = opt.filters || {};
-  this.parent = opt.parent || null;
-  this.rootQuery = opt.rootQuery || null;
-  this.children = Object.create(null);
-  this.promise = null;
+export default class EagerFetcher {
+
+  constructor({modelClass, models, eager, filters, parent, rootQuery}) {
+    this.modelClass = modelClass;
+    this.models = models;
+    this.eager = eager;
+    this.filters = filters || {};
+    this.parent = parent || null;
+    this.rootQuery = rootQuery || null;
+    this.children = Object.create(null);
+    this.promise = null;
+  }
+
+  fetch() {
+    if (this.promise) {
+      return this.promise;
+    }
+
+    if (_.isEmpty(this.models)) {
+      this.promise = Promise.resolve([]);
+      return this.promise;
+    }
+
+    let promises = [];
+
+    this.eager.forEachChild(child => {
+      let relation = this.modelClass.getRelations()[child.name];
+
+      if (!relation) {
+        throw new ValidationError({eager: 'unknown relation "' + child.name + '" in an eager expression'});
+      }
+    });
+
+    _.each(this.modelClass.getRelations(), relation => {
+      let nextEager = this.eager.childExpression(relation.name);
+
+      if (nextEager) {
+        promises.push(this._fetchRelation(relation, nextEager));
+      }
+    });
+
+    this.promise = Promise.all(promises).return(this.models);
+    return this.promise;
+  }
+
+  _fetchRelation(relation, nextEager) {
+    let ModelClass = relation.relatedModelClass;
+    let queryBuilder = ModelClass.RelatedQueryBuilder.forClass(ModelClass).childQueryOf(this.rootQuery);
+
+    relation.find(queryBuilder, this.models);
+
+    _.each(nextEager.args, filterName => {
+      let filter = this.filters[filterName];
+
+      if (!_.isFunction(filter)) {
+        throw new ValidationError({eager: 'could not find filter "' + filterName + '" for relation "' + relation.name + '"'});
+      }
+
+      filter(queryBuilder);
+    });
+
+    return queryBuilder.then(related => {
+      return this._fetchNextEager(relation, related, nextEager);
+    });
+  }
+
+  _fetchNextEager(relation, related, eager) {
+    this.children[relation.name] = new EagerFetcher({
+      modelClass: relation.relatedModelClass,
+      models: related,
+      eager: eager,
+      filters: this.filters,
+      parent: this,
+      rootQuery: this.rootQuery
+    });
+
+    return this.children[relation.name].fetch();
+  }
 }
-
-EagerFetcher.prototype.fetch = function () {
-  if (this.promise) {
-    return this.promise;
-  }
-
-  if (_.isEmpty(this.models)) {
-    this.promise = Promise.resolve([]);
-    return this.promise;
-  }
-
-  var self = this;
-  var promises = [];
-
-  this.eager.forEachChild(function (child) {
-    var relation = self.modelClass.getRelations()[child.name];
-
-    if (!relation) {
-      throw new ValidationError({eager: 'unknown relation "' + child.name + '" in an eager expression'});
-    }
-  });
-
-  _.each(this.modelClass.getRelations(), function (relation) {
-    var nextEager = self.eager.childExpression(relation.name);
-
-    if (nextEager) {
-      promises.push(self._fetchRelation(relation, nextEager));
-    }
-  });
-
-  this.promise = Promise.all(promises).return(this.models);
-  return this.promise;
-};
-
-EagerFetcher.prototype._fetchRelation = function (relation, nextEager) {
-  var self = this;
-  var ModelClass = relation.relatedModelClass;
-  var queryBuilder = ModelClass.RelatedQueryBuilder
-    .forClass(ModelClass)
-    .childQueryOf(this.rootQuery);
-
-  relation.find(queryBuilder, this.models);
-
-  _.each(nextEager.args, function (filterName) {
-    var filter = self.filters[filterName];
-
-    if (!_.isFunction(filter)) {
-      throw new ValidationError({eager: 'could not find filter "' + filterName + '" for relation "' + relation.name + '"'});
-    }
-
-    filter(queryBuilder);
-  });
-
-  return queryBuilder.then(function (related) {
-    return self._fetchNextEager(relation, related, nextEager);
-  });
-};
-
-EagerFetcher.prototype._fetchNextEager = function (relation, related, eager) {
-  this.children[relation.name] = new EagerFetcher({
-    modelClass: relation.relatedModelClass,
-    models: related,
-    eager: eager,
-    filters: this.filters,
-    parent: this,
-    rootQuery: this.rootQuery
-  });
-
-  return this.children[relation.name].fetch();
-};
-
-module.exports = EagerFetcher;
