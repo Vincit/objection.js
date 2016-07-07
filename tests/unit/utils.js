bdfbac8b07ba228c5ee0877c1ab73eb5904131d5
Sami Koskimäki <sami.koskimaki@vincit.com>
2016-07-07 23:09:57 +0300

    more tests for hiddenData

--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -34,8 +34,7 @@ describe('utils', function () {
   describe('hiddenData', function () {
     var setHiddenData = hiddenData.createHiddenDataSetter('test');
     var getHiddenData = hiddenData.createHiddenDataGetter('test');
-
-
+    
     it('should stored data under a non-enumerable $$hiddenData object', function () {
       var obj = {};
 
@@ -53,6 +52,38 @@ describe('utils', function () {
       expect(getHiddenData(obj)).to.eql({a: 'bar'});
     });
 
+    it('should not inherit hidden data from prototype', function () {
+      var obj = {};
+
+      setHiddenData(obj, 10);
+      expect(getHiddenData(obj)).to.equal(10);
+
+      var inherited = Object.create(obj);
+      expect(getHiddenData(inherited)).to.equal(undefined);
+      expect(inherited.$$hiddenData.test).to.equal(10);
+
+      setHiddenData(inherited, 20);
+      expect(getHiddenData(obj)).to.equal(10);
+      expect(getHiddenData(inherited)).to.equal(20);
+    });
+
+    it('should inherit hidden data from prototype if inheritHiddenData is used', function () {
+      var obj = {};
+
+      setHiddenData(obj, 10);
+      expect(getHiddenData(obj)).to.equal(10);
+
+      var inherited = Object.create(obj);
+      hiddenData.inheritHiddenData(obj, inherited);
+
+      expect(getHiddenData(inherited)).to.equal(10);
+      expect(inherited.$$hiddenData.test).to.equal(10);
+
+      setHiddenData(inherited, 20);
+      expect(getHiddenData(obj)).to.equal(10);
+      expect(getHiddenData(inherited)).to.equal(20);
+    });
+
   });
 
   describe('decorators', function () {
@@ -65,6 +96,21 @@ describe('utils', function () {
 
         for (var i = 0; i < 10; ++i) {
           expect(obj.test()).to.equal(0);
+        }
+      });
+
+      it('should not inherit memoized values from prototype', function () {
+        var obj = {};
+        applyDecorator(obj, 'test', createIncrementor(), memoize);
+
+        for (var i = 0; i < 10; ++i) {
+          expect(obj.test()).to.equal(0);
+        }
+
+        var inherited = Object.create(obj);
+
+        for (var i = 0; i < 10; ++i) {
+          expect(inherited.test()).to.equal(1);
         }
       });
 
