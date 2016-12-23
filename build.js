8c1b20a8b901c92631f90b7be238234f0fe442ea
Matthew McEachen <matthew+github@mceachen.org>
2016-12-23 09:35:59 -0800

    build.js forks tsc now. Pin TS to 2.0.

--- build.js
+++ build.js
@@ -1,8 +1,10 @@
 var babel = require('babel-core');
+var child_process = require('child_process');
+var fs = require('fs');
 var glob = require('glob');
 var fsExtra = require('fs-extra');
 var path = require('path');
-var fs = require('fs');
+var process = require('process');
 
 var SRC_DIR = './src';
 var DST_DIR = './lib';
@@ -55,4 +57,10 @@ for (var i = 0; i < src.length; ++i) {
 
   fsExtra.ensureDirSync(path.dirname(dst));
   fs.writeFileSync(dst, code);
+}
+
+// TypeScript requires node > 4.
+if (process.version > 'v4') {
+  // sanity-check the TypeScript definitions:
+  child_process.execSync(path.join(__dirname, "node_modules", ".bin", "tsc"), { cwd: __dirname });
 }
