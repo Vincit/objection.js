055b025d089c39903dd0b992754e1fce8b529a88
Joe Lapp <arachnojoe@gmail.com>
2018-01-23 15:29:34 -0600

    new typings header provided by @mceachen

--- typings/objection/index.d.ts
+++ typings/objection/index.d.ts
@@ -1,8 +1,15 @@
-// Type definitions for objection
-// Project: Objection.js <http://vincit.github.io/objection.js/>
-// Definitions by: Matthew McEachen <https://github.com/mceachen> & Drew R. <https://github.com/drew-r>
+// Type definitions for Objection.js
+// Project: <http://vincit.github.io/objection.js/>
+// Contributions by:
+// * Matthew McEachen <https://github.com/mceachen>
+// * Sami Koskimäki <https://github.com/koskimas>
+// * Mikael Lepistö <https://github.com/elhigu>
+// * Joseph T Lapp <https://github.com/jtlapp>
+// * Drew R. <https://github.com/drew-r>
+// * Karl Blomster <https://github.com/kblomster>
+// * And many others: See <https://github.com/Vincit/objection.js/blob/master/typings/objection/index.d.ts>
 
-// Generic type symbols:
+// PLEASE NOTE, the generic type symbols in this file follow this definition
 //   QM - queried model
 //   RM - candidate result model or model array
 //   RV - actual result value
