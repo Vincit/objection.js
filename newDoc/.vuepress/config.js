88f4e1e4fa4ff25a35161b4568fbdec21c6e3533
Ben Berman <ben.m.berman@gmail.com>
2018-05-18 00:45:41 -0400

    Add tabbed examples for different ES versions
    I had to use a combination of vue components and a plugin for the
    vuepress default theme. Vuepress's default config has the power to be
    configured heavily, but it's a little weird. If I just used the vue
    components in the md directly, it would take the children of the
    components (which should be turned into codeblocks) and make that just
    one text component, so it wouldn't get processed by markdown-it. But if
    I used markdown-it-container, the HTML replacement occurs before
    runtime.
    
    It's possible using <ClientOnly> would have worked, but I didn't try.
    This way feels more extensible, though slightly gross.

--- newDoc/.vuepress/config.js
+++ newDoc/.vuepress/config.js
@@ -44,5 +44,65 @@ module.exports = {
         ]
       }]
     }
+  },
+  markdown: {
+    config: md => {
+      let id = 0;
+      let currentTitle = null;
+      let firstContent = true;
+      const regex = /multi-language (.*)/;
+      md.use(require('markdown-it-container'), 'multi-language', {
+        render(tokens, idx) {
+          let argString;
+
+          try {
+            const matched = tokens[idx].info.trim().match(regex);
+            argString = matched[1];
+          } catch (e) {
+            return tokens[idx].info.trim();
+          }
+
+          const args = argString.split(/\s+/);
+
+          switch (args[0]) {
+            case 'example': {
+              switch (args[1]) {
+                case 'begin': {
+                  return `<tabbed-example>`;
+                }
+                case 'end': {
+                  return `</tabbed-example>`;
+                }
+                default: {
+                  throw new Error(`Failed to parse line "${tokens[idx].info}"`);
+                }
+              }
+            }
+            case 'section': {
+              const tabTitle = args[1];
+              switch (args[2]) {
+                case 'begin': {
+                  return `<tab name="${tabTitle}">`;
+                }
+                case 'end': {
+                  return `
+                        </tab>
+                  `;
+                }
+                default: {
+                  throw new Error(`Failed to parse line "${tokens[idx].info}"`);
+                }
+              }
+            }
+          }
+
+          return `
+            <pre class="language-json">
+              ${JSON.stringify(args)}
+            </pre>
+          `;
+        }
+      });
+    }
   }
-}
+};
