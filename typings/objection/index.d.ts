69eb2f71975020984bf9aefc172748882bd7314b
Will Soto <willsoto@users.noreply.github.com>
2019-10-08 08:09:46 -0400

    Add TypeScript tests for "other methods"
    Closes #1431

--- typings/objection/index.d.ts
+++ typings/objection/index.d.ts
@@ -599,6 +599,14 @@ declare namespace Objection {
     (): boolean;
   }
 
+  interface HasMethod {
+    (selector: string | RegExp): boolean;
+  }
+
+  interface ClearMethod<QB extends AnyQueryBuilder> {
+    (selector: string | RegExp): QB;
+  }
+
   interface ColumnInfoMethod<QB extends AnyQueryBuilder> {
     (): Promise<knex.ColumnInfo>;
   }
@@ -775,6 +783,7 @@ declare namespace Objection {
 
   interface ModifiersMethod<QB extends AnyQueryBuilder> {
     (modifiers: Modifiers): QB;
+    (): QB;
   }
 
   // Deprecated
@@ -1111,11 +1120,15 @@ declare namespace Objection {
     isDelete: BooleanReturningMethod;
     isRelate: BooleanReturningMethod;
     isUnrelate: BooleanReturningMethod;
+    isInternal: BooleanReturningMethod;
     hasWheres: BooleanReturningMethod;
     hasSelects: BooleanReturningMethod;
     // Deprecated
     hasEager: BooleanReturningMethod;
     hasWithGraph: BooleanReturningMethod;
+
+    has: HasMethod;
+    clear: ClearMethod<this>;
 
     clearSelect: IdentityMethod<this>;
     clearOrder: IdentityMethod<this>;
