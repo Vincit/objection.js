056411b81aedb6410da1aebbc83f9b3c62529d79
Dean Mauro <DeanMauro@users.noreply.github.com>
2021-02-23 00:19:16 +0000

    Added traverseAsync typing to ModelClass as well

--- typings/objection/index.d.ts
+++ typings/objection/index.d.ts
@@ -1506,6 +1506,12 @@ declare namespace Objection {
       models: Model | Model[],
       traverser: TraverserFunction
     ): void;
+    traverseAsync(models: Model | Model[], traverser: TraverserFunction): Promise<void>;
+    traverseAsync(
+      filterConstructor: ModelConstructor<Model>,
+      models: Model | Model[],
+      traverser: TraverserFunction
+    ): Promise<void>;
 
     beforeFind(args: StaticHookArguments<any>): any;
     afterFind(args: StaticHookArguments<any>): any;
