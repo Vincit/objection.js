7c8a2825343d59c1bb32881fffb0806c8118c97b
Sami Koskimäki <koskomi@gmail.com>
2021-11-05 17:03:26 +0200

    remove unused stuff

--- tests/integration/find.js
+++ tests/integration/find.js
@@ -2413,12 +2413,12 @@ module.exports = (session) => {
         it('should return undefined if the model has no parents', async () => {
           const parent = await Model1.query().findById(2);
 
-          const result1 = await parent.$relatedQuery('model1Relation1').debug();
+          const result1 = await parent.$relatedQuery('model1Relation1');
           expect(result1).to.be.equal(undefined);
 
-          const result2 = await Model1.relatedQuery('model1Relation1')
-            .for(Model1.relatedQuery('model1Relation1').for(parent))
-            .debug();
+          const result2 = await Model1.relatedQuery('model1Relation1').for(
+            Model1.relatedQuery('model1Relation1').for(parent)
+          );
           expect(result2).to.eql([]);
         });
 
