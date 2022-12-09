737322fdfe85df551f2dd8acb562ae94e98e4577
Neil Gentleman <ngentleman@gmail.com>
2022-12-09 14:33:32 -0800

    add unit test coverage for ValueBuilder
    copying the same _createRawArgs pattern from ReferenceBuilder to give
    the test easy access to the generated SQL.

---
+++ tests/unit/queryBuilder/ValueBuilder.js
@@ -0,0 +1,30 @@
+const expect = require('expect.js');
+const { val, Model } = require('../../../');
+const { ValueBuilder } = require('../../../lib/queryBuilder/ValueBuilder');
+
+function toRawArgs(ref) {
+  return ref._createRawArgs(Model.query());
+}
+
+describe('ValueBuilder', () => {
+  it('should create ValueBuilder', () => {
+    let builder = val('Awwww.ItWorks');
+    expect(builder instanceof ValueBuilder).to.be.ok();
+    expect(toRawArgs(builder)).to.eql(['?', 'Awwww.ItWorks']);
+  });
+
+  it('should allow casting', () => {
+    let builder = val('100').castBigInt();
+    expect(toRawArgs(builder)).to.eql(['CAST(? AS bigint)', '100']);
+  });
+
+  it('should stringify when casting to json', () => {
+    let builder = val({ value: 100 }).castJson();
+    expect(toRawArgs(builder)).to.eql(['CAST(? AS jsonb)', '{"value":100}']);
+  });
+
+  it('should expand arrays', () => {
+    let builder = val([1, 2, 3]).asArray();
+    expect(toRawArgs(builder)).to.eql(['ARRAY[?, ?, ?]', [1, 2, 3]]);
+  });
+});
