Vincit/objection.js

commit 6b5b300c9beb31ed8e5b2d9a1ad13d83efea222d

Include ajv-formats by default (#2453)
Closes #2218, #2142, #2146, #2147, #2170

lib/model/AjvValidator.js
@@ -1,16 +1,10 @@
 'use strict';
 
+const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
 const { Validator } = require('./Validator');
 const { ValidationErrorType } = require('../model/ValidationError');
 const { isObject, once, cloneDeep: lodashCloneDeep, omit } = require('../utils/objectUtils');
-
-const getAjv = once(() => {
-  try {
-    return require('ajv');
-  } catch (err) {
-    throw new Error('Optional ajv dependency not installed. Please run `npm install ajv --save`');
-  }
-});
 
 class AjvValidator extends Validator {
   static init(self, conf) {
@@ -19,8 +13,6 @@ class AjvValidator extends Validator {
     self.ajvOptions = Object.assign({}, conf.options, {
       allErrors: true,
     });
-
-    const Ajv = getAjv();
 
     // Create a normal Ajv instance.
     self.ajv = new Ajv(
@@ -43,8 +35,13 @@ class AjvValidator extends Validator {
     // A cache for the compiled validator functions.
     self.cache = new Map();
 
-    conf.onCreateAjv(self.ajv);
-    conf.onCreateAjv(self.ajvNoDefaults);
+    const setupAjv = (ajv) => {
+      addFormats(ajv);
+      conf.onCreateAjv(ajv);
+    };
+
+    setupAjv(self.ajv);
+    setupAjv(self.ajvNoDefaults);
   }
 
   beforeValidate({ json, model, options, ctx }) {

commit 7a4084e2c5ea62d162777f6b47e0f2ad0c6b879c

Only add AJV formats if they weren't added already
Caused by #2453
Closes #2481

lib/model/AjvValidator.js
@@ -36,8 +36,11 @@ class AjvValidator extends Validator {
     self.cache = new Map();
 
     const setupAjv = (ajv) => {
-      addFormats(ajv);
       conf.onCreateAjv(ajv);
+      // Only add AJV formats if they weren't added in user-space already
+      if (!ajv.formats['date-time']) {
+        addFormats(ajv);
+      }
     };
 
     setupAjv(self.ajv);

tests/unit/model/AjvValidator.js
@@ -1,3 +1,4 @@
+const addFormats = require('ajv-formats');
 const { AjvValidator, Model } = require('../../../');
 const expect = require('expect.js');
 
@@ -61,6 +62,16 @@ describe('AjvValidator', () => {
       ],
     };
 
+    const schema3 = {
+      type: 'object',
+      properties: {
+        date: {
+          type: 'string',
+          format: 'date-time',
+        },
+      },
+    };
+
     it('should remove required fields from definitions', () => {
       const validator = new AjvValidator({ onCreateAjv: () => {} });
       const validators = validator.getValidator(modelClass('test', schema), schema, true);
@@ -79,6 +90,23 @@ describe('AjvValidator', () => {
       });
       const validators = validator.getValidator(modelClass('test', schema2), schema2, true);
       expect(validators.schema.required).to.eql(['foo']);
+    });
+
+    it('should add ajv formats by default', () => {
+      expect(() => {
+        const validator = new AjvValidator({ onCreateAjv: () => {} });
+        validator.getValidator(modelClass('test', schema3), schema3, true);
+      }).to.not.throwException();
+    });
+
+    it('should not throw errors when adding formats in onCreateAjv hook', () => {
+      expect(() => {
+        new AjvValidator({
+          onCreateAjv: (ajv) => {
+            addFormats(ajv);
+          },
+        });
+      }).to.not.throwException();
     });
 
     it('should handle empty definitions', () => {